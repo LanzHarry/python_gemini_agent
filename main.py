926fb0002ccb4f7366e9ac8631d1b5d909ee62cc
Harry Lanz <lanzharry97@gmail.com>
2026-01-15 22:44:49 +0000

    add user prompt at run and ensure client closes

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
+
+# parse input arguments
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args() # gives access to args.user_prompt
 
 # load dotenv and read the gemini API key
 load_dotenv()
@@ -14,7 +20,7 @@ client = genai.Client(api_key=api_key)
 
 model_response = client.models.generate_content(
         model = "gemini-2.5-flash",
-        contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+        contents = args.user_prompt)
 
 if model_response.usage_metadata is not None:
     prompt_token_count = model_response.usage_metadata.prompt_token_count
@@ -25,3 +31,5 @@ else:
 print(f"Prompt tokens: {prompt_token_count}\nResponse tokens: {candidates_token_count}")
 
 print(model_response.text)
+
+client.close()
