387cd4a01f305a525545e673bca3927fc3f117c2
Harry Lanz <lanzharry97@gmail.com>
2026-01-18 19:37:34 +0000

    add file info getter function

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,18 +2,38 @@ import os
 
 def get_files_info(working_directory, directory="."):
     # validate path to directory is inside working directory
-    abs_path = os.path.abspath(working_directory)
+    working_dir_abs = os.path.abspath(working_directory)
 
     # construct full path from abs path and relative path
-    target_dir = os.path.normpath(os.path.join(abs_path, directory))
+    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
 
-    # check if target_dir falls within abs_path to working directory
-    valid_target_dir = os.path.commonpath([abs_path, target_dir]) == abs_path
+    # check if target_dir falls within working_dir_abs to working directory
+    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
 
     if not valid_target_dir:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
-    if not os.path.isdir(directory):
+    if not os.path.isdir(target_dir):
         return f'Error: "{directory}" is not a directory'
+
+    try:
+        dir_contents = os.listdir(target_dir)
+    except FileNotFoundError:
+        return f'Error: "{target_dir}" not found'
+    except NotADirectoryError:
+        return f'Error: "{target_dir}" is not a directory'
+    except PermissionError:
+        return f'Error: Program does not have permission to access "{target_dir}"' 
+
+    dir_contents_string = ""
     
-        
+    for elt in dir_contents:
+        try:
+            elt_path = os.path.join(target_dir, elt) 
+            fsize = os.path.getsize(elt_path) 
+            is_dir = os.path.isdir(elt_path)
+            dir_contents_string += f"- {elt}: file_size={fsize}, is_dir={is_dir}\n"
+        except Exception as e:
+            return f'Error: {str(e)}'
+
+    return dir_contents_string
