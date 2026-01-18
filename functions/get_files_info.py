0dffc4ce11bf2d69e17f8a4ee0b9d4cf982b0a39
Harry Lanz <lanzharry97@gmail.com>
2026-01-18 19:45:30 +0000

    add error simple error handling

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,14 +1,17 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-    # validate path to directory is inside working directory
-    working_dir_abs = os.path.abspath(working_directory)
+    try:
+        # validate path to directory is inside working directory
+        working_dir_abs = os.path.abspath(working_directory)
 
-    # construct full path from abs path and relative path
-    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+        # construct full path from abs path and relative path
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
 
-    # check if target_dir falls within working_dir_abs to working directory
-    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+        # check if target_dir falls within working_dir_abs to working directory
+        valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+    except Exception as e:
+        return f"Error checking working and target directory: {e}"
 
     if not valid_target_dir:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
